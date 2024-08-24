c56d9a57dc6b68b9115e6309a522d27b16e3083d
Islam Hani <72826869+islamhani97@users.noreply.github.com>
2024-08-25 02:57:51 +0300

    Enhance UI and themes

--- app/src/main/java/com/islam97/android/apps/movies/presentation/utils/Bindings.kt
+++ app/src/main/java/com/islam97/android/apps/movies/presentation/utils/Bindings.kt
@@ -1,10 +1,13 @@
 package com.islam97.android.apps.movies.presentation.utils
 
+import android.graphics.drawable.LayerDrawable
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.islam97.android.apps.movies.R
 import com.islam97.android.apps.movies.domain.models.Movie
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -23,10 +26,24 @@ fun movieReleaseDate(textView: TextView, releaseDate: String?) {
 
 @BindingAdapter(value = ["ratingProgress"])
 fun ratingProgress(progressBar: ProgressBar, rating: Double?) {
-    progressBar.progress = rating?.let {
+    val progress = rating?.let {
         (it * 10).roundToInt()
     } ?: 0
 
+    progressBar.progress = progress
+
+    val color = ContextCompat.getColor(
+        progressBar.context, when (progress) {
+            in 0..29 -> R.color.progressColorLow
+            in 30..69 -> R.color.progressColorMedium
+            else -> R.color.progressColorHigh
+        }
+    )
+
+    val progressDrawable = progressBar.progressDrawable
+    if (progressDrawable is LayerDrawable) {
+        progressDrawable.findDrawableByLayerId(R.id.progress_indicator).setTint(color)
+    }
 }
 
 @BindingAdapter(value = ["rating"])
@@ -38,20 +55,21 @@ fun rating(textView: TextView, rating: Double?) {
 
 @BindingAdapter(value = ["movieDetails"])
 fun movieDetails(textView: TextView, movie: Movie?) {
-    textView.text =
-        "${getReleaseDate(movie?.releaseDate)} | ${movie?.originalLanguage?.uppercase()}"
+    textView.text = StringBuilder().append(getReleaseDate(movie?.releaseDate)).append(" | ")
+        .append(movie?.originalLanguage?.uppercase()).toString()
 }
 
 
 private fun loadImage(imageView: ImageView, url: String) {
-    Glide.with(imageView).load(url).into(imageView)
+    Glide.with(imageView).load(url).placeholder(R.mipmap.placeholder).into(imageView)
 }
 
 fun getReleaseDate(releaseDate: String?): String {
     return releaseDate?.let { dateString ->
         try {
-            val date = SimpleDateFormat("yyyy-MM-dd", Locale("en")).parse(dateString)
-            SimpleDateFormat("MMM dd, yyyy", Locale("en")).format(date)
+            SimpleDateFormat("yyyy-MM-dd", Locale("en")).parse(dateString)?.let { date ->
+                SimpleDateFormat("MMM dd, yyyy", Locale("en")).format(date)
+            } ?: ""
         } catch (e: Exception) {
             ""
         }
